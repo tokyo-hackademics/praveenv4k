8cb6efd629c2e72b7924f3572f1413ec72229297
Praveenkumar Vasudevan <praveenv4k@gmail.com>
2015-05-16 07:53:33 +0200

    added gitignore, added evaluate service

--- facilitator_engine/src/facilitator_client.cpp
+++ facilitator_engine/src/facilitator_client.cpp
@@ -1,16 +1,9 @@
 #include "ros/ros.h"
 #include "facilitator_engine/GetQuestion.h"
+#include "facilitator_engine/Evaluate.h"
 #include <cstdlib>
 
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "facilitator_client");
-  if (argc != 3)
-  {
-    ROS_INFO("usage: facilitator_client id msg");
-    return 1;
-  }
-
+int testQuestionService(char **argv){
   ros::NodeHandle n;
   ros::ServiceClient client = n.serviceClient<facilitator_engine::GetQuestion>("GetQuestion");
   facilitator_engine::GetQuestion srv;
@@ -23,8 +16,37 @@ int main(int argc, char **argv)
   else
   {
     ROS_ERROR("Failed to call service GetQuestion");
+  }
+}
+
+int testEvaluateService(int id, const char* answer){
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<facilitator_engine::Evaluate>("Evaluate");
+  facilitator_engine::Evaluate srv;
+  srv.request.id = id;
+  srv.request.answer = answer;
+  if (client.call(srv))
+  {
+    ROS_INFO("Confidence: %lf, %s", srv.response.confidence,srv.response.comment.c_str());
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service Evaluate");
+  }
+  ROS_INFO("testEvaluateService: OK");
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "facilitator_client");
+  if (argc != 3)
+  {
+    ROS_INFO("usage: facilitator_client id msg");
     return 1;
   }
+
+  testQuestionService(argv);
+  testEvaluateService(1,"Answer is 42!");
 
   return 0;
 }
--- facilitator_engine/src/facilitator_server.cpp
+++ facilitator_engine/src/facilitator_server.cpp
@@ -1,21 +1,34 @@
 #include "ros/ros.h"
 #include "facilitator_engine/GetQuestion.h"
+#include "facilitator_engine/Evaluate.h"
 
 bool getQuestion(facilitator_engine::GetQuestion::Request  &req,
          facilitator_engine::GetQuestion::Response &res)
 {
   res.question = "What is the capital of Japan?";
-  ROS_INFO("request: x=%ld, y=%s", (long int)req.id, req.msg.c_str());
+  ROS_INFO("request: id=%ld, msg=%s", (long int)req.id, req.msg.c_str());
   ROS_INFO("sending back response: [%s]", res.question.c_str());
   return true;
 }
+
+bool evaluate(facilitator_engine::Evaluate::Request  &req,
+         facilitator_engine::Evaluate::Response &res)
+{
+  res.comment = "Correct Answer! Congratulations!";
+  res.confidence = 1.0f;
+  ROS_INFO("request: id=%ld, answer=%s", (long int)req.id, req.answer.c_str());
+  ROS_INFO("sending back response- Confidence : [%lf], Comment: [%s]",res.confidence, res.comment.c_str());
+  return true;
+}
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "facilitator_server");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("GetQuestion", getQuestion);
+  ros::ServiceServer questionService = n.advertiseService("GetQuestion", getQuestion);
+  ros::ServiceServer evaluateService = n.advertiseService("Evaluate", evaluate);
   ROS_INFO("Facilitator Server Ready.");
   ros::spin();
 
